319e68601eb3b20b4f1677c890283ed6dc1e12b6
WellingtonAlvesLira <wellingtonalvessiqueira@hotmail.com>
2022-07-27 11:49:20 -0300

    Termino da validação dos dados/feedback usuário

--- src/components/cep/CepPage.vue
+++ src/components/cep/CepPage.vue
@@ -3,6 +3,12 @@
     <v-container fluid>
       <v-row justify="center" class="mt-5">
         <v-col cols="12" md="4" lg="4" class="text-center mt-7">
+          <v-alert shaped outlined type="success" v-model="verificadorInputSucess">
+            {{msgVerificadorInputSucess}}
+            <v-btn text color="green" @click="verificadorInputSucess = false">
+              Fechar
+            </v-btn>
+          </v-alert>
           <h1 class="text-center mt-10">Buscar CEP</h1>
           <form class="mb-5">
             <v-text-field
@@ -12,6 +18,10 @@
               label="CEP"
               placeholder="Pesquisar CEP"
             ></v-text-field>
+            <v-alert dense outlined type="error" v-model="verificadorInput">
+              {{ msgVerificadorInput }}
+              <v-btn text @click="verificadorInput = false"> Fechar </v-btn>
+            </v-alert>
             <v-btn class="mr-4" color="primary" @click="pesquisar">
               Pesquisar
             </v-btn>
@@ -20,7 +30,10 @@
           <v-row>
             <v-col cols="12" md="8">
               <!-- Enviando a variavel book, para o ListaCep para da inicio as funcionalidades externas-->
-              <ListaCep class="mt-5 text-left" :cepResult="cepResult"></ListaCep>
+              <ListaCep
+                class="mt-5 text-left"
+                :cepResult="cepResult"
+              ></ListaCep>
             </v-col>
           </v-row>
         </v-col>
@@ -32,7 +45,7 @@
 <script>
 import ListaCep from "@/components/cep/ListaCep.vue";
 
-import api from '@/api/api.js';
+import api from "@/api/api.js";
 
 export default {
   name: "CepPage",
@@ -40,25 +53,40 @@ export default {
   mixins: [api],
   data() {
     return {
+      verificadorInput: false,
+      verificadorInputSucess: false,
+      msgVerificadorInput: "",
+      msgVerificadorInputSucess: "",
       cepInput: "",
       cepResult: "",
+      maxInput: 8,
     };
   },
   methods: {
-    pesquisar(){
-      if(this.cepInput){
-        this.get(`/ws/${this.cepInput}/json/`).then((resposta) =>{
-          
-        this.cepResult = resposta.data;
-        
-        })
+    pesquisar() {
+      if (this.cepInput) {
+        this.get(`/ws/${this.cepInput}/json/`).then((resposta) => {
+          this.cepResult = resposta.data;
+          this.msgVerificadorInputSucess = "Busca realizada com sucesso!"
+          this.verificadorInputSucess = true;
+        });
+        if (
+          this.cepInput.length < this.maxInput ||
+          this.cepInput.length > this.maxInput
+        ) {
+          this.msgVerificadorInput = "Digite apenas 8 números";
+          this.verificadorInput = true;
+        }
+      } else {
+        this.msgVerificadorInput = "Campo em branco. Insira um cep válido!";
+        this.verificadorInput = true;
       }
-      
     },
-    limpar(){
-      this.cepInput = "";
-    }
-  }
+    limpar() {
+      this.cepInput = "",
+      this.cepResult = ""
+    },
+  },
 };
 </script>
 
